65759c647e91ab1ec68c4a097e8f822b7f486f6a
MinnieZSky <130220779+MinnieZSky@users.noreply.github.com>
2023-12-08 14:49:15 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -86,30 +86,6 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 
 export const DEFAULT_MODELS = [
   {
-    name: "gpt-4",
-    available: false,
-  },
-  {
-    name: "gpt-4-0314",
-    available: false,
-  },
-  {
-    name: "gpt-4-0613",
-    available: false,
-  },
-  {
-    name: "gpt-4-32k",
-    available: false,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: false,
-  },
-  {
-    name: "gpt-4-32k-0613",
-    available: false,
-  },
-  {
     name: "gpt-4-1106-preview",
     available: true,
   },
@@ -118,29 +94,9 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
-    name: "gpt-3.5-turbo",
-    available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-0301",
-    available: false,
-  },
-  {
-    name: "gpt-3.5-turbo-0613",
-    available: false,
-  },
-  {
     name: "gpt-3.5-turbo-1106",
     available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-16k",
-    available: false,
-  },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
-    available: false,
-  },
+  }
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
